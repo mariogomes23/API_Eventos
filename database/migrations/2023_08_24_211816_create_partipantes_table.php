91f1c6153d228e792c989311e2d605e28de2a1ef
mario gomes <markkennedy232323@gmail.com>
2023-08-24 22:34:11 +0100

    adicionar migracao

--- database/migrations/2023_08_24_211816_create_partipantes_table.php
+++ database/migrations/2023_08_24_211816_create_partipantes_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Evento;
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('partipantes', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(User::class);
+            $table->foreignIdFor(Evento::class);
             $table->timestamps();
         });
     }
